ce331f1dc7a0a6656cc270efc3c8169a9a63ed46
Gonza10Co <gonzalo_correa_g@hotmail.com>
2022-08-14 17:52:30 -0500

    add dependencies axios, react-redux, react-router-dom, redux, redux-thunk... add folders and files for redux and add loading action, reducer, create store, add provider, all connected and working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 // import { useState } from 'react';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
 	// const [count, setCount] = useState(0);
 
 	return (
 		<div className='App'>
+			<Home/>
 			<h1>Hola mundo 😋!!</h1>
 		</div>
 	);
---
+++ client/src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import redLoading from './loading';
+
+const reducer = combineReducers({
+  redLoading,
+});
+
+export default reducer;

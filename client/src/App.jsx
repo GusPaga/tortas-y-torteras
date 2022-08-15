653165dda57ec1ffdf198f8c73060bb8d6f9845e
Gonza10Co <gonzalo_correa_g@hotmail.com>
2022-08-15 15:26:35 -0500

    add details page and fetch details

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import Detail from './pages/Detail';
 
 function App() {
-
 	return (
 		<div className='App'>
 			<Switch>
 				<Route exact path='/' component={Home} />
-				{/* <Route exact path='/bases/:id' component={Detail} /> */}
+				<Route exact path='/:id' component={Detail} />
 			</Switch>
 		</div>
 	);

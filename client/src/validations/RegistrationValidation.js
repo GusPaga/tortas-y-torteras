f03df7a4387809bc2747d0efb9addc7872c6484c
unknown <omarramirez5876@gmail.com>
2022-08-15 17:53:49 -0500

    creating registration form

--- client/src/validations/RegistrationValidation.js
+++ client/src/validations/RegistrationValidation.js
@@ -3,7 +3,7 @@ export const validate = (input) => {
     const error = {}
 
     if(!input.name) error.name = 'Name is required'
-    if(!input.lastname) error.lastname = 'Summary is required'
+    if(!input.lastname) error.lastname = 'Lastname is required'
     if(!/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(input.email)) error.email = 'It has to be an email'
     if(!input.user) error.user = 'User is required'
     if(!input.address) error.address = 'Address is required'

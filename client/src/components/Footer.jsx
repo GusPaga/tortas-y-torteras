f92e597d1c4b018b18d30bd32ab84bda4d1db513
Gonza10Co <gonzalo_correa_g@hotmail.com>
2022-08-16 07:17:05 -0500

    fix route app, add footer without css

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import './Footer.css';
+
+export default function Footer() {
+	return (
+		<div className='footer'>
+			<div className='footer-text'>Calle 73 # 17-181 Bogotá - Colombia</div>
+			<span className='foorer-instagram'>
+				<i className='fa-brands fa-instagram'></i>
+			</span>
+			<span className='foorer-facebook'>
+				<i className='fa-brands fa-facebook'></i>
+			</span>
+			<span className='foorer-tiktok'>
+				<i className='fa-brands fa-tiktok'></i>
+			</span>
+			<span className='foorer-whatsapp'>
+				<i className='fa-brands fa-whatsapp'></i>
+			</span>
+		</div>
+	);
+}

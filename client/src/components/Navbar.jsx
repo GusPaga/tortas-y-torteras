8e26606a6d99f40f61361396d6c717fe5b2fa58c
Gonza10Co <gonzalo_correa_g@hotmail.com>
2022-08-17 16:10:06 -0500

    registration fixed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
 	return (
 		<div className='bar-header'>
 			<span className='bar-brush-icon'>
-				<i className='fa-solid fa-palette'></i>
+				<Link to='/'>
+					<i className='fa-solid fa-palette'></i>
+				</Link>
 			</span>
 			<div className='bar-heading'>Cakes&Bases</div>
 			<span className='bar-bag-icon'>
@@ -13,7 +16,9 @@ export default function Navbar() {
 			<span className='bar-user-icon'>
 				<i className='fa-solid fa-circle-user'></i>
 			</span>
-			<div className='bar-login-text'>Log In</div>
+			<Link to='/registration' className='bar-login-text'>
+				Log In
+			</Link>
 		</div>
 	);
 }

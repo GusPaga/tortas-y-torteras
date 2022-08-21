22b366647903ad76f7c8c75b894f594e60d2408b
jfarroyo291 <j-a-m291@hotmail.com>
2022-08-21 11:29:33 -0500

    productForm send data

--- client/src/components/ProductForm.jsx
+++ client/src/components/ProductForm.jsx
@@ -16,11 +16,12 @@ const ProductForm = () => {
 		cakeTrail: '',
 		turntable: '',
 	});
+	const [file, setFile] = useState(); // eslint-disable-line no-unused-vars
 	const dispatch = useDispatch();
 	const [input, setInput] = useState({
 		name: '',
 		description: '',
-		imageMain: '',
+		// imageMain: '',
 		imagesDetail: [],
 		collection: '',
 		artist: '',
@@ -28,7 +29,7 @@ const ProductForm = () => {
 			{ quantity: 0, productTypeName: "Cake Tray" },
       { quantity: 0, productTypeName: "Turntable" }
 		],
-		colors: [],
+		color: [],
 	});
 
 	const countSelected = () => {
@@ -36,7 +37,7 @@ const ProductForm = () => {
 		const btnText = document.querySelector('.btn-text');
 		checked.length
 			? (btnText.innerText = `${checked.length} Selected`)
-			: (btnText.innerText = 'Select Color');
+			: (btnText.innerText = 'Select Temperament');
 	};
 
 	const handleOnClickDiv = () => {
@@ -45,6 +46,7 @@ const ProductForm = () => {
 	};
 
 	const OnClickItem = e => {
+		setInput({...input,color:[...input.color, e.target.id]})
 		const li = // si el click es en algun span, el elemento es li
 			e.target.classList[0] === 'item' ? e.target : e.target.parentElement;
 		li.classList.toggle('checked');
@@ -75,13 +77,30 @@ const ProductForm = () => {
 		);
 	};
 
+	const handleImage = e => {
+		const imageAsB64 = URL.createObjectURL(e.target.files[0])
+		setFile(imageAsB64);
+		/* setError(
+			validateProduct({
+				...input,
+				[e.target.name]: e.target.value,
+			})
+		) */
+	};
+
 	const handleChangeImages = e => {
-		if(input.imagesDetail.includes(e.target.value)) {alert('Image exists already')}
+		if(input.imagesDetail.includes(URL.createObjectURL(e.target.files[0]))) {alert('Image exists already')}
 		else {
 			setInput({
 				...input,
-				imagesDetail: [...input.imagesDetail, e.target.value]
-			})
+				imagesDetail: [...input.imagesDetail, URL.createObjectURL(e.target.files[0])]
+			});
+			/* setError(
+				validateProduct({
+					...input,
+					[e.target.name]: e.target.value,
+				})
+			) */
 		};
 	};
 
@@ -98,21 +117,37 @@ const ProductForm = () => {
 	const handleSubmit = async e => {
 		e.preventDefault();
 		const newProduct = { ...input };
+		newProduct.imageMain = file;
 		newProduct.stock[0].quantity = stock.cakeTrail;
 		newProduct.stock[1].quantity = stock.turntable;
-		console.log(newProduct);
+		console.log('ANTES', newProduct);
 		if (!newProduct.name || !!Object.keys(error).length)
 			alert('Some fields are missing');
-		try {
-			await axios.post('http://localhost:3001/product', newProduct);
-		} catch (error) {}
+			try {
+				const newInput = {
+					name: newProduct.name,
+					description: newProduct.description,
+					collection: newProduct.collection,
+					imageMain:newProduct.imageMain,
+					imagesDetail:newProduct.imagesDetail,
+					artist: newProduct.artist,
+					stock: newProduct.stock,
+					colors:[
+						{hex: newProduct.color[0].split(",")[0],name:newProduct.color[0].split(",")[1]},
+						{hex:newProduct.color[1].split(",")[0],name:newProduct.color[1].split(",")[1]},
+						{hex:newProduct.color[2].split(",")[0],name:newProduct.color[2].split(",")[1]}]
+				}
+				console.log('DESPUES', newInput)
+				await axios.post('http://localhost:3001/products', newInput);
+			// });
+			} catch (error) {}
 	};
 
 	useEffect(() => {}, []);
 
 	return (
 		<div className='productFormContainer'>
-			<form onSubmit={handleSubmit}>
+			<form onSubmit={handleSubmit} encType='multipart/form-data'>
 				<span className='uppercase tracking-wide text-black text-center text-lg font-bold mb-2'>
 					Product Form
 				</span>
@@ -148,13 +183,21 @@ const ProductForm = () => {
 				</label>
 				{error.imageMain && <span className="text-red-500 text-xs italic">{error.imageMain}</span>}
 				<input
+					data-toggle="tooltip"
+					data-placement="bottom"
 					type='file'
 					className='w-full bg-gray-100 text-black border border-gray-200 rounded-md py-1 px-4 mb-3'
 					name='imageMain'
 					placeholder='Image main...'
 					value={input.imageMain}
-					onChange={handleChange}
+					onChange={handleImage}
 				/>
+				<label
+					// class="custom-file-label"
+					// for="customFileLangHTML"
+					data-browse="Elegir archivo"
+					>* Seleccionar una foto
+					</label>
 
 				<label className='uppercase tracking-wide text-black text-xs font-bold mb-2'>
 					Images Details
@@ -180,21 +223,21 @@ const ProductForm = () => {
 						onChange={handleChange}
 					>
 						<FormControlLabel
-							value='abstract'
+							value='Abstract'
 							control={<Radio />}
 							label='Abstract'
 						/>
 						<FormControlLabel
-							value='flowers'
+							value='Flowers'
 							control={<Radio />}
 							label='Flowers'
 						/>
 						<FormControlLabel
-							value='butterflies'
+							value='Butterflies'
 							control={<Radio />}
 							label='Butterflies'
 						/>
-						<FormControlLabel value='other' control={<Radio />} label='Other' />
+						<FormControlLabel value='Other' control={<Radio />} label='Other' />
 					</RadioGroup>
 				</FormControl>
 
@@ -205,7 +248,7 @@ const ProductForm = () => {
 					type='number'
 					className='w-full bg-gray-100 text-black border border-gray-200 rounded-md py-1 px-4 mb-3'
 					name='cakeTrail'
-					placeholder='1, 2 or more...'
+					placeholder='0, 1, 2 or more...'
 					value={stock.cakeTrail}
 					onChange={handleChangeStock}
 				/>
@@ -217,7 +260,7 @@ const ProductForm = () => {
 					type='number'
 					className='w-full bg-gray-100 text-black border border-gray-200 rounded-md py-1 px-4 mb-3'
 					name='turntable'
-					placeholder='1, 2 or more...'
+					placeholder='0, 1, 2 or more...'
 					// value={input.stock[1].quantity}
 					value={stock.turntable}
 					onChange={handleChangeStock}
@@ -250,6 +293,7 @@ const ProductForm = () => {
 						<li key={c.id} className='item' name='colors' onClick={OnClickItem}>
 							<span
 								className='checkbox'
+								id={[c.hex,c.name]}
 								style={{
 									backgroundColor: `${c.hex}`,
 									borderColor: `${c.hex}`,
@@ -257,7 +301,7 @@ const ProductForm = () => {
 							>
 								<i className='fa-solid fa-check check-icon'></i>
 							</span>
-							<span id={`item${c.id}`} className='item-text'>
+							<span /* id={`item${c.id}`} */ id={[c.hex,c.name]} className='item-text'>
 								{c.name}
 							</span>
 						</li>
--- client/src/validations/productValidation.js
+++ client/src/validations/productValidation.js
@@ -1,6 +1,6 @@
 // const noEmpty = /\S+/;
-const letters = /^[a-z]+$/i;
-const validImage = /.(gif|jpeg|jpg|png)$/i;
+const letters = /^([a-zA-Z]+)(\s[a-zA-Z]+)*$/;
+// const validImage = /.(gif|jpeg|jpg|png)$/i;
 
 export const validateProduct = input => {
 	const error = {};
@@ -9,12 +9,6 @@ export const validateProduct = input => {
 		error.name = 'Name is required. Alphabetic field';
 	if (input.description.length > 50 || input.description.length < 5)
 		error.description = 'Description cannot be longer than 50 characters';
-	if (!validImage.test(input.imageMain))
-		error.imageMain = 'This image format is not supported';
-	if (input.imageMain.length === 0) error.imageMain = 'Please select an image';
-	if (!validImage.test(input.imagesDetail))
-		error.imagesDetail = 'This image format is not supported';
-	if (input.imageMain.length === 0) error.imageMain = 'Please select an image';
 	if (!letters.test(input.artist)) error.artist = 'Artist is required';
 	return error;
 };

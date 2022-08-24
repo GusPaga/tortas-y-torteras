a4beecd8c3f3c13d7e4d86304524a0f634a329c7
Andrés Salazar <andrespjm@gmail.com>
2022-08-24 00:32:42 -0500

    Finished form signup user style | frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,14 @@
-
 import { Route, Switch } from 'react-router-dom';
+import { SignUp } from './components/auth/SignUp';
+// import SignInUp from './components/formsUsers/Index';
 import Navbar from './components/Navbar';
+import Page404 from './components/Pag404';
 import ProductForm from './components/ProductForm';
 import ShoppingCart from './components/ShoppingCart';
-import SignInUp from './components/formsUsers/Index';
 import Try from './components/Try';
 import Detail from './pages/Detail';
 import Home from './pages/Home';
 import Landing from './pages/Landing';
-import Page404 from './components/Pag404';
-
 
 function App() {
 	return (
@@ -21,7 +20,7 @@ function App() {
 				<Route exact path='/addproduct' component={ProductForm} />
 				<Route exact path='/shop/shoppingCart' component={ShoppingCart} />
 				<Route exact path='/bases/try' component={Try} />
-				<Route exact path='/user/auth' component={SignInUp} />
+				<Route exact path='/signup' component={SignUp} />
 				<Route exact path='/:id' component={Detail} />
 				<Route path='/' component={Page404} />
 			</Switch>

62b28ffc849186e16c03a432929ff02f35e80422
jfarroyo291 <j-a-m291@hotmail.com>
2022-08-25 19:06:59 -0500

    add forms for client dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,8 @@ import { useDispatch } from 'react-redux';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { SignIn } from './components/auth/SignIn';
 import { SignUp } from './components/auth/SignUp';
-// import SignInUp from './components/formsUsers/Index';
+import { EditUserProfile } from './components/formsUsers/EditUserProfile';
+import { ChangePassword } from './components/formsUsers/ChangePassword';
 import Navbar from './components/Navbar';
 import Page404 from './components/Pag404';
 import ProductForm from './components/ProductForm';
@@ -43,6 +44,8 @@ function App() {
 				{/* <Route exact path='/bases/signin' component={SignInSide} /> */}
 				<Route exact path='/bases/try' component={Try} />
 				<Route exact path='/signup' component={SignUp} />
+				<Route exact path='/user/edit' component={EditUserProfile} />
+				<Route exact path='/user/changepassword' component={ChangePassword} />
 				<Route
 					exact
 					path='/signin'
